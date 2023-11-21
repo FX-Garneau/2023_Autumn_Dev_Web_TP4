e706e735f8b24917d3285e4723b7b4aca674f482
georgi <gegav001@edu.cegepgarneau.cagit config --global user.name georgigit config --global user.email gegav001@edu.cegepgarneau.ca>
2023-11-20 21:57:32 -0500

    ajotu du des fonction de base dans le fichier javascript et fonction-utilitaires

--- TP4-Gestion des tâches_/js/fonctions-utilitaires.js
+++ TP4-Gestion des tâches_/js/fonctions-utilitaires.js
@@ -1,6 +1,42 @@
 "use strict";
 
+
+
 /**
  * Fonctions utilitaires utilisables dans n'importe quel projet
  */
 
+
+
+
+
+function creerCard(pImage,pTitre,pDEscription,pEstAvecBouton,pElementHtmlBouton)
+{
+    let divContentLECard = document.createElement('div');
+
+    divContentLECard.className="card";
+
+    for(let data of DATA_TACHES.detailsTache)
+    {
+        // header du card:
+        let CardHeader = document.createElement('div');
+        let img = document.createElement('img');
+        
+        //body du card:
+        let CardBody = document.createElement('div');
+
+        // footer du card:
+        let cardFooter = document.createElement('div');
+        let btnSupprimer = document.createElement('button');
+
+        img.src="./images/check2-square.svg";
+
+        divContentLECard.appendChild(CardHeader);
+        CardHeader.appendChild(img);
+        divContentLECard.appendChild(CardBody);
+        //CardBody.appendChild();
+        divContentLECard.appendChild(cardFooter);
+        cardFooter.appendChild(btnSupprimer);
+    }
+
+}
---
+++ TP4-Gestion des tâches_/js/javascript.js
@@ -0,0 +1,43 @@
+"use strict";
+// Global DATA_TACHES, fonctions_utilitaires.js
+
+/* George :
+Todo : aussi
+- creer une animation sur l'affichage
+- rafraichir l'affichage du diagrame 
+*/
+
+
+
+function creeDoneesPourGraphique()
+{
+
+}
+
+
+
+function ChargerEtAfficherDonnerDiagrammeEtCards(){
+
+
+}
+
+function AfficherCardsTaches(){
+    let DivCard = document.getElementById("LesCards");
+    DivCard.innerHTML="";
+    let Cards =
+    DivCard.className="row";
+
+}
+
+function SupprimerTache(){
+
+}
+
+-
+
+
+function verifierSiDependanceExiste(){
+
+}
+
+
