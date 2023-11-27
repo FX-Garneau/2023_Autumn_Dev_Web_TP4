3a2422e5f7ad8d248fcd3dd0c3f692bfac0af8ce
georgi :) <gogo.burga@gmail.com>
2023-11-27 07:44:22 -0500

    ajout des distrubution des taches dans le fichier cmd, plus ajout des commentaire des fonction dans le fichier fonction-utilitaire.js

--- TP4-Gestion des tâches_/docs/infos-pour-prof.md
+++ TP4-Gestion des tâches_/docs/infos-pour-prof.md
@@ -3,11 +3,54 @@
 # La lettre de notre équipe et nos noms
 
 Équipe no.
-Étudiant 1 :
-Étudiant 2 :
+Étudiant 1 : Georgi Gavrailov
+Étudiant 2 : Ulric Huot
 
 # Notre séparation des tâches
+##  Ulric : 
+(etudiant 2)
+ • initialisation 
+• Utiliser une fenêtre modale Bootstrap 5 
+qui contient un formulaire pour éditer les 
+données d’une tâche 
+• recupererTacheSelectionneeDansDiagram
+meDeGantt 
+• sauvegarderChangementsTache + 
+Utilisation d’une fonction anonyme ou 
+fléchée 
+• supprimerTache (utilisation d’un attribut 
+HMTL personnalisé) 
+• calculerAvancement 
+• arreterMinuterie 
+• ajusterAvancementTacheSelonMinuterie 
+• convertirJoursEnMillisecondes 
+• convertirMillisecondesEnJours 
+
+     
+
+## George :
+(etudiant 1)
+• initialisation 
+• creerDonneesPourGraphique 
+• chargerEtAfficherDonneesDiagrammeEtCa
+rds 
+• afficherCardsTaches 
+• supprimerTache (utilisation d’un attribut 
+HMTL personnalisé) 
+• creerCard 
+• verifierSiDependanceExiste 
+• Rafraîchir l’affichage du diagramme lors 
+de l’événement « resize » de la fenêtre +  
+Utilisation d’une fonction anonyme ou 
+fléchée 
+• Créer une animation sur l’affichage des 
+cards
+
+
 
 # Les couleurs choisies
 
 # Autres commentaires
+
+
+
--- TP4-Gestion des tâches_/js/fonctions-utilitaires.js
+++ TP4-Gestion des tâches_/js/fonctions-utilitaires.js
@@ -1,33 +1,33 @@
 "use strict";
 
 
-
 /**
- * Fonctions utilitaires utilisables dans n'importe quel projet
+ * 
+ * @param {string} pImage resoit l'image 
+ * @param {string} pTitre recois le titre du card 
+ * @param {string} pDEscription recois la description 
+ * @param {boolean} pEstAvecBouton  s'il y a un bouton ou non
+ * @param {*} pElementHtmlBouton je ne sais pas
  */
-
-
-
-
-
 function creerCard(pImage,pTitre,pDEscription,pEstAvecBouton,pElementHtmlBouton)
 {
-    let divContentLECard = document.createElement('div');
+    let divContentLECard = document.createElement("div");
 
     divContentLECard.className="card";
 
     for(let data of DATA_TACHES.detailsTache)
     {
         // header du card:
-        let CardHeader = document.createElement('div');
-        let img = document.createElement('img');
-        
+        let CardHeader = document.createElement("div");
+        let img = document.createElement("img");
+        let h2 = document.createElement("h2");
+
         //body du card:
-        let CardBody = document.createElement('div');
+        let CardBody = document.createElement("div");
 
         // footer du card:
-        let cardFooter = document.createElement('div');
-        let btnSupprimer = document.createElement('button');
+        let cardFooter = document.createElement("div");
+        let btnSupprimer = document.createElement("button");
 
         img.src="./images/check2-square.svg";
 
@@ -37,6 +37,9 @@ function creerCard(pImage,pTitre,pDEscription,pEstAvecBouton,pElementHtmlBouton)
         //CardBody.appendChild();
         divContentLECard.appendChild(cardFooter);
         cardFooter.appendChild(btnSupprimer);
+
+        img.src=pImage;
+    
     }
 
 }
