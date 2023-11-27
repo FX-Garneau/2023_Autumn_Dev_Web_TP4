e43aa1e2eff38a5ec79b5fd2bd4e735edee930dc
georgi :) <gogo.burga@gmail.com>
2023-11-27 09:35:29 -0500

    ajout des classe dans la fonction creercard

--- TP4-Gestion des tâches_/js/fonctions-utilitaires.js
+++ TP4-Gestion des tâches_/js/fonctions-utilitaires.js
@@ -2,57 +2,67 @@
 /*global DATA_TACHES */
 
 /**
- * 
- * @param {string} pImage resoit l'image 
- * @param {string} pTitre recois le titre du card 
- * @param {string} pDEscription recois la description 
+ *
+ * @param {string} pImage resoit l'image
+ * @param {string} pTitre recois le titre du card
+ * @param {string} pDEscription recois la description
  * @param {boolean} pEstAvecBouton  s'il y a un bouton ou non
  * @param {*} pElementHtmlBouton je ne sais pas
  * @returns {Element} retourne la div contenat le card
  */
-function creerCard(pImage,pTitre,pDEscription,pEstAvecBouton,pElementHtmlBouton)
-{
-    let divContentLECard = document.createElement("div");
+function creerCard(
+  pImage,
+  pTitre,
+  pDEscription,
+  pEstAvecBouton,
+  pElementHtmlBouton
+) {
+  let divContentLECard = document.createElement("div");
+  divContentLECard.className = "card";
+  // header du card:
+  let CardHeader = document.createElement("div");
+  CardHeader.className = "card-header";
+  let img = document.createElement("img");
+  img.className = "card-image";
+  let h2 = document.createElement("h2");
+  h2.classList.add("card-title");
+  let ul = document.createElement("ul");
 
-        divContentLECard.className="card";
-        // header du card:
-        let CardHeader = document.createElement("div");
-        CardHeader.classList.add("");
-        let img = document.createElement("img");
-        let h2 = document.createElement("h2");
-        let ul = document.createElement("ul");
+  for (let data of DATA_TACHES.detailsTache) {
+    let li = document.createElement("li");
+    li.textContent = data.id;
+    li.textContent = data.titre;
+    li.textContent = data.dateDebut;
+    li.textContent = data.dateFin;
+    li.textContent = data.dureeEnNbJours;
+    li.textContent = data.pctComplete;
+    li.textContent = data.dependances;
+    ul.appendChild(li);
+  }
 
-        for(let data of DATA_TACHES.detailsTache)
-        {
-            let li = document.createElement("li");
-            li.textContent = data.id;
-            li.textContent = data.titre;
-            li.textContent = data.dateDebut;
-            li.textContent = data.dateFin;
-            li.textContent = data.dureeEnNbJours;
-            li.textContent = data.pctComplete;
-            li.textContent = data.dependances;
-            ul.appendChild(li);
-        }
-        
-        //body du card:
-        let CardBody = document.createElement("div");
-        h2.textContent=pTitre;
-        h2.className="";
+  //body du card:
+  let CardBody = document.createElement("div");
+  CardBody.classList.add("card-body");
+  h2.textContent = pTitre;
+  h2.className = "card-title";
 
+  // footer du card:
+  let cardFooter = document.createElement("div");
+  cardFooter.className = "card-footer";
+  let btnSupprimer = document.createElement("button");
+  btnSupprimer.textContent = "Supprimer";
+  btnSupprimer.className = "btn btn-warning";
 
-        // footer du card:
-        let cardFooter = document.createElement("div");
-        let btnSupprimer = document.createElement("button");
-        
-        img.src=pImage;
+  img.src = pImage;
+  img.alt = "photo de la card";
 
+  divContentLECard.appendChild(CardHeader);
+  CardHeader.appendChild(img);
+  divContentLECard.appendChild(CardBody);
+  CardBody.appendChild(ul);
 
-        divContentLECard.appendChild(CardHeader);
-        CardHeader.appendChild(img);
-        divContentLECard.appendChild(CardBody);
-        CardBody.appendChild(h2);
-        divContentLECard.appendChild(cardFooter);
-        cardFooter.appendChild(btnSupprimer);
-        return divContentLECard;
-}   
+  CardBody.appendChild(h2);
+  divContentLECard.appendChild(cardFooter);
+  cardFooter.appendChild(btnSupprimer);
+  return divContentLECard;
+}
--- TP4-Gestion des tâches_/js/javascript.js
+++ TP4-Gestion des tâches_/js/javascript.js
@@ -1,62 +1,42 @@
 "use strict";
 /* global DATA_TACHES, creerCard*/
 
-
-
 /**
  * fonction qui initialise le fichier de base et appelle
  *  les fonction de base lors du chargement de la page
  */
-function Initialisation(){
-    AfficherCardsTaches();
-    ChargerEtAfficherDonnerDiagrammeEtCards();
+function Initialisation() {
+  AfficherCardsTaches();
+  ChargerEtAfficherDonnerDiagrammeEtCards();
 }
 
+function creeDoneesPourGraphique() {}
 
-function creeDoneesPourGraphique()
-{
-
-}
-
-
-
-function ChargerEtAfficherDonnerDiagrammeEtCards(){
-
-
-}
+function ChargerEtAfficherDonnerDiagrammeEtCards() {}
 
 /**
  * appelle la fonction creercard du fichier fonction-utilitaire.js
  *  pour les lier et les afficher a l'ecran de l'utilisateur
  */
-function AfficherCardsTaches(){
-    
-    let DivCard = document.getElementById("LesCards");
-
-    for(let data of DATA_TACHES.detailsTache)
-    {
-
-        for(let datas of DATA_TACHES.taches)
-            {
-            let Cards = creerCard("./images/check2-square.svg",datas.titre,data.detailsTache,data.detailsTache,"..");
-            DivCard.appendChild(Cards);
-        }
-
+function AfficherCardsTaches() {
+  let DivCard = document.getElementById("LesCards");
+  DivCard.className = "d-flex row";
+  for (let data of DATA_TACHES.detailsTache) {
+    for (let datas of DATA_TACHES.taches) {
+      let Cards = creerCard(
+        "./images/check2-square.svg",
+        datas.titre,
+        data.detailsTache,
+        data.detailsTache,
+        ".."
+      );
+      DivCard.appendChild(Cards);
     }
-    DivCard.className="row";
-
+  }
 }
 
-function SupprimerTache(){
+function SupprimerTache() {}
 
-}
+function verifierSiDependanceExiste() {}
 
--
-
-
-function verifierSiDependanceExiste(){
-
-};
-
-
-window.addEventListener("load",Initialisation);
+window.addEventListener("load", Initialisation);
