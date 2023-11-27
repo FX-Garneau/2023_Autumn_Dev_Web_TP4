eee78d3a8caafd7d5f0eea3dc5aeb22eb4bc2724
Ulric Huot <ulric.huot@gmail.com>
2023-11-27 09:34:58 -0500

    lisibilite

--- TP4-Gestion des tâches_/js/javascript.js
+++ TP4-Gestion des tâches_/js/javascript.js
@@ -1,27 +1,20 @@
 "use strict";
 /* global DATA_TACHES, creerCard*/
 
-
-
 /**
  * fonction qui initialise le fichier de base et appelle
  *  les fonction de base lors du chargement de la page
  */
-function Initialisation(){
-    AfficherCardsTaches();
-    ChargerEtAfficherDonnerDiagrammeEtCards();
+function Initialisation() {
+   AfficherCardsTaches();
+   ChargerEtAfficherDonnerDiagrammeEtCards();
 }
 
-
-function creeDoneesPourGraphique()
-{
+function creeDoneesPourGraphique() {
 
 }
 
-
-
-function ChargerEtAfficherDonnerDiagrammeEtCards(){
-
+function ChargerEtAfficherDonnerDiagrammeEtCards() {
 
 }
 
@@ -29,34 +22,21 @@ function ChargerEtAfficherDonnerDiagrammeEtCards(){
  * appelle la fonction creercard du fichier fonction-utilitaire.js
  *  pour les lier et les afficher a l'ecran de l'utilisateur
  */
-function AfficherCardsTaches(){
-    
-    let DivCard = document.getElementById("LesCards");
+function AfficherCardsTaches() {
+   let DivCard = document.getElementById("LesCards");
 
-    for(let data of DATA_TACHES.detailsTache)
-    {
+   for (let data of DATA_TACHES.detailsTache)
+      for (let datas of DATA_TACHES.taches)
+         DivCard.appendChild(creerCard("./images/check2-square.svg", datas.titre, data.detailsTache, data.detailsTache, ".."));
+}
 
-        for(let datas of DATA_TACHES.taches)
-            {
-            let Cards = creerCard("./images/check2-square.svg",datas.titre,data.detailsTache,data.detailsTache,"..");
-            DivCard.appendChild(Cards);
-        }
-
-    }
-    DivCard.className="row";
+function SupprimerTache() {
 
 }
 
-function SupprimerTache(){
+function verifierSiDependanceExiste() {
 
 }
 
--
 
-
-function verifierSiDependanceExiste(){
-
-};
-
-
-window.addEventListener("load",Initialisation);
+window.addEventListener("load", Initialisation);
